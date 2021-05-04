b7fa72cc0c756d4b0e714dc0b5253a425f2c42e7
Leimar <leimarferreira100@gmail.com>
2021-05-04 12:11:22 -0300

    Updating purchase resource.
    Now it returns user and product objects instead of user and product ids.

--- Resources/PurchaseResource.cs
+++ Resources/PurchaseResource.cs
@@ -9,12 +9,12 @@ namespace ProjetoDFS.Resources
         public int Id { get; set; }
         public float Value { get; set; }
         public DateTime Date { get; set; }
-        public PaymentMethod PaymentMethod { get; set; }
-        public PurchaseStatus Status { get; set; }
+        public string PaymentMethod { get; set; }
+        public string Status { get; set; }
         public string Note { get; set; }
         public string PostalCode { get; set; }
         public string Address { get; set; }
-        public int ProductId { get; set; }
-        public int UserId { get; set; }
+        public ProductResource Product { get; set; }
+        public UserResource Buyer { get; set; }
     }
 }
